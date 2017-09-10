788e51598582c79b6a141d63d48781e3417083ac
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-10 11:59:59 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -8,7 +8,7 @@ if (index.name === "Susan") {
 
 
 
-if (abs(index.height) === 70) {
+if (round(index.height) === 70) {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
